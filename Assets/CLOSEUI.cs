2aa624e5b6d8cba522a0994d6fa0c0389a036a0f
ines said <inessaid@mail.usf.edu>
2021-09-19 03:16:31 -0400

    hiding UI after set up

---
+++ Assets/CLOSEUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CLOSEUI : MonoBehaviour
+{
+    public GameObject UItohide;
+    // Start is called before the first frame update
+   public void hide()
+    {
+        UItohide.SetActive(false);
+    }
+}
